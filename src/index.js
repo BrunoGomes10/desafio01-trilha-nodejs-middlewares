c7c2a58f43b403e9df256bc8c171687a25381c4e
Bruno Gomes <brunogomes.gomes@gmail.com>
2021-03-20 14:37:58 -0400

    Desafio Finalizado Com Sucesso !!!

--- src/index.js
+++ src/index.js
@@ -10,19 +10,78 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found !!!' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+
+  const { user } = request;
+
+  const userPro = user.pro;
+  const qtyTodos = user.todos.length;
+
+  if (qtyTodos >= 10 && !userPro) {
+    return response.status(403).json({ error: 'Usuario Atingiu limite do Plano !!!' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found !!!' });
+  }
+
+  const uuidValidate = validate(id);
+
+  if (!uuidValidate) {
+    return response.status(400).json({ error: 'Id - Não é do tipo "uuid".' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found !!!' });
+  }
+
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found !!!' });
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
